d7a748b8b4163c0ca7519737cc6009f1f5420e37
alexmo16 <alexismoreldev@gmail.com>
2018-12-30 15:03:57 -0500

    start of websocket and https ready

--- server/Heylo/src/routes/LoginController.js
+++ server/Heylo/src/routes/LoginController.js
@@ -1,8 +1,5 @@
 let express = require('express');
-let validator = require('../authentification/Validator');
 let router = express.Router();
-
-router.all('/login', validator);
 
 router.get('/login', function(req, res, next) {
   res.send('log yourself');
