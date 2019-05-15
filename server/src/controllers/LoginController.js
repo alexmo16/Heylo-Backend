33ce53a2c4dea1985173db5fc11eac9598d435d9
alexmo16 <alexismoreldev@gmail.com>
2019-05-15 06:49:23 -0400

    put password in payload instead of query params lel

--- server/src/controllers/LoginController.js
+++ server/src/controllers/LoginController.js
@@ -6,16 +6,20 @@ let users = require('../services/users/Users');
 let utils = require('../utils/Utils');
 
 // TODO: re-think user login with userID.
-router.get('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
     if (req.headers.g_token) return res.sendStatus(403);
 
-    if (!req.query.email) return res.sendStatus(400);
-    if (!req.query.password) return res.sendStatus(400);
+    console.log(req.body.email);
 
-    let email = req.query.email;
-    let password = req.query.password;
+    if (!req.body.email) return res.sendStatus(400);
+    if (!req.body.password) return res.sendStatus(400);
+
+    let email = req.body.email;
+    let password = req.body.password;
     users.findUserByEmail(email, function(err, user) {
         if (err) return next(err);
+
+        if (!user.password) return res.sendStatus(403);
 
         if (!user) return res.sendStatus(401);
 
