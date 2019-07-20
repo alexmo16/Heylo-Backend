47bb89855d5f97c154ae6adc808392218166265d
alexmo16 <alexismoreldev@gmail.com>
2019-07-20 17:30:51 -0400

    fix NODE_ENV and helmet.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let helmet = require('helmet');
 let cors = require('cors');
+let errorHandler = require('errorHandler');
 
 require('./env');
 require('./db');
@@ -11,20 +12,24 @@ let routes = require('./routes');
 let app = express();
 
 // Third-party modules for our server.
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(helmet({
     frameguard: {
         action: 'deny'
     }
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors({
     origin: process.env.ORIGIN
 }));
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(logger('dev'));
+    app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+}
 
 // Connect all our routes to our application.
 routes(app);
--- server/env.js
+++ server/env.js
@@ -1,6 +1,8 @@
 /** Load differents environment variables */
-if (process.env.production) {
+if (process.env.NODE_ENV === 'production') {
     require('dotenv').config({ path: 'config/production/.env' });
+    process.stdout.write('production mode...\n');
 } else {
     require('dotenv').config({ path: 'config/development/.env' });
+    process.stdout.write('development mode...\n');
 }
